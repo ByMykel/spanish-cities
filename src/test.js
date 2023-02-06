2196b42e62ebe1e95c03807887eb1329188c6524
Mihail Dragnev <bymykel5@gmail.com>
2023-02-06 18:53:47 +0100

    refactor: remove test file.

--- src/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var assert_1 = require("assert");
-describe('test', function () {
-    it('should pass', function () {
-        (0, assert_1["default"])(true);
-    });
-});
