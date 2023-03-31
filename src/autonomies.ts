dccfc04418ed4bbb856539562acd29733affad1c
Mihail Dragnev <bymykel5@gmail.com>
2023-03-31 20:38:47 +0200

    feat: ability to filter by name.

--- src/autonomies.ts
+++ src/autonomies.ts
@@ -5,12 +5,18 @@ import { optionsAutonomy, Autonomy } from "./types/index";
  * Returns an array of autonomies that match the specified filter criteria.
  * @param options (optional) An object with options.
  * @param options.code A string or number representing the code of the autonomy to filter by.
+ * @param options.name A string representing the name of the autonomy to filter by.
  */
 export const autonomies = (options: optionsAutonomy = {}): Autonomy[] => {
-  const { code } = options;
+  const { code, name } = options;
 
   return data.filter((item: Autonomy) => {
     if (code !== undefined && item.code != code) return false;
+
+    if (name !== undefined && !item.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())) {
+      return false;
+    }
+
     return true;
   });
 }
--- src/provinces.ts
+++ src/provinces.ts
@@ -6,17 +6,19 @@ import { optionsProvince, Province } from "./types";
  * @param options (optional) An object with options.
  * @param options.code A string or number representing the code of the province to filter by.
  * @param options.code_autonomy A string or number representing the code of the autonomy to filter by.
+ * @param options.name A string representing the name of the province to filter by.
  */
 export const provinces = (options: optionsProvince = {}): Province[] => {
-  const attributes = ["code", "code_autonomy"]
-    .filter(option => options[option as keyof optionsProvince] !== undefined);
+  const { code, code_autonomy, name } = options;
 
   return data.filter((item: Province) => {
-    for (const attribute of attributes) {
-      if (item[attribute as keyof optionsProvince] != options[attribute as keyof optionsProvince]) {
-        return false;
-      }
+    if (code !== undefined && item.code != code) return false;
+    if (code_autonomy !== undefined && item.code_autonomy != code_autonomy) return false;
+
+    if (name !== undefined && !item.name.toLocaleLowerCase().includes(name.toLocaleLowerCase())) {
+      return false;
     }
+
     return true;
   });
 };
