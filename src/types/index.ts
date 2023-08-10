dfe60077cf79d37b4136c7d57b1f8e5b9772364d
Mihail Dragnev <bymykel5@gmail.com>
2023-08-10 10:40:43 +0200

    feat: add more attributes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,11 @@
+type Nullable<T> = T | null;
+type Coordinates = { latitude: Nullable<number>, longitude: Nullable<number> };
+type Links = {
+  wikipedia: Nullable<string>;
+  geohack: Nullable<string>;
+  website: Nullable<string>;
+}
+
 export interface optionsAutonomy {
   code?: string | number;
   name?: string;
@@ -19,6 +27,11 @@ export interface OptionsCity {
 export interface Autonomy {
   code: string;
   name: string;
+  flag: Nullable<string>;
+  coat_of_arms: Nullable<string>;
+  hymn: Nullable<string>;
+  coordinates: Coordinates;
+  links: Links;
 }
 
 export interface Province {
